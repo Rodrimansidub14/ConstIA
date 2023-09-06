3b0e4a8ee93c579db6cbb47381db99fb05b2c0c1
Andre <andremt2003@gmail.co>
2023-09-06 15:16:33 -0600

    intento de avances

--- app/src/main/java/com/example/myapplication/MainViews.kt
+++ app/src/main/java/com/example/myapplication/MainViews.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
