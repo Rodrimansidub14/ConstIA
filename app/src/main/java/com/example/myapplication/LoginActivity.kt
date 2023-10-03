2ce899cb5a36dfc90dc3115e39f968b5501b39ab
Andre <andremt2003@gmail.co>
2023-10-02 21:33:49 -0600

    Login api funcionando utilizando firebase. terminado

--- app/src/main/java/com/example/myapplication/LoginActivity.kt
+++ app/src/main/java/com/example/myapplication/LoginActivity.kt
@@ -2,148 +2,81 @@ package com.example.myapplication
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.text.withStyle
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.example.myapplication.ui.theme.MyApplicationTheme
+import android.text.TextUtils
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 
-class LoginActivity : ComponentActivity() {
+class LoginActivity: AppCompatActivity() {
+
+    private lateinit var login1: Button
+    private lateinit var email: EditText
+    private lateinit var password: EditText
+    private lateinit var registrarse: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            MyApplicationTheme {
-                LoginView()
-            }
+        setContentView(R.layout.activity_login)
+
+        login1 = findViewById(R.id.login1)
+        email = findViewById(R.id.email)
+        password = findViewById(R.id.password)
+        registrarse = findViewById(R.id.registrarse)
+
+        registrarse.setOnClickListener {
+            val intent = Intent(this@LoginActivity, RegisterActivity::class.java)
+            startActivity(intent)
         }
-    }
-
-    @OptIn(ExperimentalMaterial3Api::class)
-    @Composable
-    fun LoginView() {
-        val context = LocalContext.current
-        val imageResId = R.drawable.logofin
-        val backgroundColor = colorResource(id = R.color.backgroundcolorviews)
-        val secundaryColor = colorResource(id = R.color.secundary)
-        val principalColor = colorResource(id = R.color.principal)
-
-        Surface(color = backgroundColor, modifier = Modifier.fillMaxSize()) {
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(16.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                val text = buildAnnotatedString {
-                    withStyle(style = SpanStyle(color = secundaryColor)) {
-                        append("Iniciar")
-                    }
-                    withStyle(style = SpanStyle(color = principalColor)) {
-                        append("Sesion")
-                    }
+        login1.setOnClickListener {
+            when {
+                TextUtils.isEmpty(email.text.toString().trim { it <= ' ' }) -> {
+                    Toast.makeText(
+                        this@LoginActivity,
+                        "Por favor ingrese un email",
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
-
-                Text(
-                    text = text,
-                    style = TextStyle(
-                        fontSize = 40.sp,
-                        fontWeight = FontWeight.Bold
-                    ),
-                    modifier = Modifier.padding(8.dp)
-                )
-                Image(
-                    painter = painterResource(id = imageResId),
-                    contentDescription = null,
-                    modifier = Modifier
-                        .size(150.dp)
-                        .padding(top = 24.dp, bottom = 16.dp)
-                )
-
-                TextEntry("Correo Electrónico")
-                TextEntry("Contraseña", true)
-
-                Button(
-                    onClick = {
-                        val intent = Intent(context, HomeScreenActivity::class.java)
-                        context.startActivity(intent)
-                    },
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(top = 16.dp, bottom = 24.dp)
-                ) {
-                    Text(text = "Iniciar Sesión")
+                TextUtils.isEmpty(password.text.toString().trim { it <= ' ' }) -> {
+                    Toast.makeText(
+                        this@LoginActivity,
+                        "Por favor ingrese una contraseña",
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
+                else -> {
+                    var userEmail: String = email.text.toString().trim { it <= ' ' }
+                    var userPassword: String = password.text.toString().trim { it <= ' ' }
 
-                Row(
-                    horizontalArrangement = Arrangement.Center,
-                    verticalAlignment = Alignment.CenterVertically,
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    Text("¿Aún no tienes una cuenta?")
-                    Spacer(modifier = Modifier.width(4.dp))
-                    Text(
-                        text = "Regístrate",
-                        color = principalColor,
-                        modifier = Modifier.clickable {
-                            val intent = Intent(context, RegisterActivity::class.java)
-                            context.startActivity(intent)
+                    FirebaseAuth.getInstance().signInWithEmailAndPassword(userEmail, userPassword)
+                        .addOnCompleteListener { task ->
+                            if (task.isSuccessful) {
+                                Toast.makeText(
+                                    this@LoginActivity,
+                                    "Se ha logueado exitosamente",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                                val intent = Intent(this@LoginActivity, HomeScreenActivity::class.java)
+                            //    intent.flags =
+                                //    Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                                intent.putExtra("user_id", FirebaseAuth.getInstance().currentUser!!.uid)
+                                intent.putExtra("email_id", userEmail)
+                                startActivity(intent)
+                                finish()
+                            } else {
+                                Toast.makeText(
+                                    this@LoginActivity,
+                                    task.exception!!.message.toString(),
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
                         }
-                    )
                 }
             }
         }
     }
 
-    @OptIn(ExperimentalMaterial3Api::class)
-    @Composable
-    fun TextEntry(hint: String, password: Boolean = false) {
-        var textValue by remember { mutableStateOf(TextFieldValue()) }
 
-        OutlinedTextField(
-            value = textValue,
-            onValueChange = {
-                textValue = it
-            },
-            placeholder = { Text(hint) },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 4.dp)
-                .height(60.dp),
-            visualTransformation = if (password) PasswordVisualTransformation() else VisualTransformation.None
-        )
-    }
 
-    @Preview
-    @Composable
-    fun LoginViewPreview() {
-        MyApplicationTheme {
-            Surface {
-                LoginView()
-            }
-        }
-    }
 }
--- app/src/main/java/com/example/myapplication/RegisterActivity.kt
+++ app/src/main/java/com/example/myapplication/RegisterActivity.kt
@@ -1,173 +1,89 @@
-// Main activity for the sign up screen
 package com.example.myapplication
+
 import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.text.withStyle
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.example.myapplication.ui.theme.MyApplicationTheme
-
-import androidx.compose.runtime.*
+import android.text.TextUtils
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseUser
 
 
+class RegisterActivity : AppCompatActivity() {
 
+    private lateinit var registro1: Button
+    private lateinit var email: EditText
+    private lateinit var password: EditText
+    private lateinit var loger: Button
 
-class RegisterActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            SignUpView() {
+        setContentView(R.layout.activity_register)
 
-            }
+        registro1 = findViewById(R.id.registro1)
+        email = findViewById(R.id.email)
+        password = findViewById(R.id.password)
+        loger = findViewById(R.id.loger)
+
+        loger.setOnClickListener {
+            val intent = Intent(this@RegisterActivity, LoginActivity::class.java)
+            startActivity(intent)
         }
-    }
 
-    @OptIn(ExperimentalMaterial3Api::class)
-    @Composable
-    fun SignUpView(onSignUpClick: () -> Unit) {
-        val context = LocalContext.current
-        val imageResId = R.drawable.logofin
-        val backgroundColor = colorResource(id = R.color.backgroundcolorviews)
-        val secundaryColor = colorResource(id = R.color.secundary)
-        val principalColor = colorResource(id = R.color.principal)
-
-        Surface(color = backgroundColor, modifier = Modifier.fillMaxSize()) {
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(16.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                val text = buildAnnotatedString {
-                    withStyle(style = SpanStyle(color = secundaryColor)) {
-                        append("Regis")
-                    }
-                    withStyle(style = SpanStyle(color = principalColor)) {
-                        append("trarse")
-                    }
+        registro1.setOnClickListener {
+            when {
+                TextUtils.isEmpty(email.text.toString().trim { it <= ' ' }) -> {
+                    Toast.makeText(
+                        this@RegisterActivity,
+                        "Por favor ingrese un email",
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
 
-                Text(
-                    text = text,
-                    style = TextStyle(
-                        fontSize = 40.sp,
-                        fontWeight = FontWeight.Bold
-                    ),
-                    modifier = Modifier.padding(8.dp)
-                )
-
-                Image(
-                    painter = painterResource(id = imageResId),
-                    contentDescription = null,
-                    modifier = Modifier
-                        .size(150.dp)
-                        .padding(8.dp)
-                )
-
-                TextEntry("Nombre")
-                TextEntry("Apellido")
-                TextEntry("Correo Electrónico")
-                TextEntry("Contraseña", true)
-                TextEntry("Confirmación de Contraseña", true)
-
-                Button(
-                    onClick = {
-                        val intent = Intent(context, LoginActivity::class.java)
-                        context.startActivity(intent)
-                    },
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(bottom = 8.dp)
-                ) {
-                    Text(text = "Registrarse")
+                TextUtils.isEmpty(password.text.toString().trim { it <= ' ' }) -> {
+                    Toast.makeText(
+                        this@RegisterActivity,
+                        "Por favor ingrese una contraseña",
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
 
-                Row(
-                    horizontalArrangement = Arrangement.Center,
-                    verticalAlignment = Alignment.CenterVertically,
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    Text("¿Ya tienes una cuenta?")
-                    Spacer(modifier = Modifier.width(4.dp))
-                    Text(
-                        text = "Login",
-                        color = principalColor,
-                        modifier = Modifier.clickable {
-                            val intent = Intent(context, LoginActivity::class.java)
-                            context.startActivity(intent)
+                else -> {
+                    val userEmail: String = email.text.toString().trim { it <= ' ' }
+                    val userPassword: String = password.text.toString().trim { it <= ' ' }
+
+                    FirebaseAuth.getInstance().createUserWithEmailAndPassword(userEmail, userPassword)
+                        .addOnCompleteListener { task ->
+                            if (task.isSuccessful) {
+                                val firebaseUser: FirebaseUser? = task.result?.user
+
+                                Toast.makeText(
+                                    this@RegisterActivity,
+                                    "Se ha registrado exitosamente",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+
+                                val intent = Intent(this@RegisterActivity, LoginActivity::class.java)
+                                intent.flags =
+                                    Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                                intent.putExtra("user_id", firebaseUser?.uid)
+                                intent.putExtra("email_id", userEmail)
+                                startActivity(intent)
+                                finish()
+                            } else {
+                                Toast.makeText(
+                                    this@RegisterActivity,
+                                    task.exception?.message.toString(),
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
                         }
-                    )
                 }
-            }
-        }
-    }
-
-    @OptIn(ExperimentalMaterial3Api::class)
-    @Composable
-    fun TextEntry(hint: String, password: Boolean = false) {
-        var textValue by remember { mutableStateOf(TextFieldValue()) }
-
-        OutlinedTextField(
-            value = textValue,
-            onValueChange = {
-                textValue = it
-            },
-            placeholder = { Text(hint) },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 8.dp)
-                .height(60.dp),
-            visualTransformation = if (password) PasswordVisualTransformation() else VisualTransformation.None
-        )
-    }
-
-    @Preview
-    @Composable
-    fun SignUpViewPreview() {
-        MyApplicationTheme {
-            Surface {
-                SignUpView(
-                    onSignUpClick = { /* Manejar clic en registro */ }
-                )
             }
         }
     }
 }
-
