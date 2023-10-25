ae374599d70828f46f07df188436f6f73941faf4
Joaquin Campos <cam22155@uvg.edu.gt>
2023-10-25 00:33:40 -0600

    Implementacion API nutri
    Problemas con la api_key

--- app/src/main/java/com/example/myapplication/RetrofitService.kt
+++ app/src/main/java/com/example/myapplication/RetrofitService.kt
@@ -6,18 +6,24 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface RetrofitService {
-    @GET("format=full&nutrients=203")
-    suspend fun alimentos(){
-        //@Query("api_key") apiKey: String,
+    @GET("/v1/food/fdcld?format=full&nutrients=203")
+    suspend fun alimentosrecomendados(){
+        @Query("api_key") apiKey: String,
         //@Query("region") region: String
 
     }
+    //codigo que ira en actividad donde se sugiere comidas
+    /*val servicio = RetrofitServiceFactory.makeRetrofitService()
+    lifecycleScope.launch{
+        service.alimentosrecomendados()
+    }
+    * */
 
 }
 object RetrofitServiceFactory{
     fun makeRetrofitService(): RetrofitService{
         return Retrofit.Builder()
-            .baseUrl("https://api.nal.usda.gov/fdc/v1/food/fdcld?")
+            .baseUrl("https://api.nal.usda.gov/fdc")
             .addConverterFactory(GsonConverterFactory.create())
             .build().create(RetrofitService::class.java)
 
