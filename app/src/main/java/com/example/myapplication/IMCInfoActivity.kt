ce130a770f9165cd237c083ecabd12b2b20f05bb
Gabriel Paz <paz221087@uvg.edu.gt>
2023-11-18 19:27:18 -0600

    Implementacion de Toolbars y mejoras en el UI

--- app/src/main/java/com/example/myapplication/IMCInfoActivity.kt
+++ app/src/main/java/com/example/myapplication/IMCInfoActivity.kt
@@ -16,6 +16,23 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
+import android.content.Intent
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.myapplication.ui.theme.MyApplicationTheme
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
+
 
 class IMCInfoActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,6 +44,7 @@ class IMCInfoActivity : ComponentActivity() {
         }
     }
 
+    @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     fun IMCInfoScreen() {
         val context = LocalContext.current
@@ -34,48 +52,113 @@ class IMCInfoActivity : ComponentActivity() {
         val backgroundColor = colorResource(id = R.color.backgroundcolorviews)
         val secundaryColor = colorResource(id = R.color.secundary)
         val principalColor = colorResource(id = R.color.principal)
-        val imageResId = R.drawable.logoconstia
+        val imageResId = R.drawable.logofin
+        val appBarColor = Color(0xFF0A3D38)
+        val colorDeFondo = Color(0xFFF4F4F4)
 
-        Column(
+
+
+        Box(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(16.dp)
-                .background(backgroundColor), // Set the background color of the column
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+                .background(colorDeFondo)
         ) {
+            Scaffold(
 
-            Text(
-                text = "INFORMACION IMC",
-                style = MaterialTheme.typography.headlineMedium.copy(color = principalColor), // Set the color of the headline
-                modifier = Modifier.padding(bottom = 16.dp)
-            )
+                topBar = {
+                    CustomTopAppBar(
+                        title = "INFORMACION IMC",
+                        backgroundColor = appBarColor,
+                        onBack = {
+                            val intent = Intent(context, NutritionScreenActivity::class.java)
+                            intent.flags =
+                                Intent.FLAG_ACTIVITY_CLEAR_TOP // Esto asegura no acumular actividades
+                            context.startActivity(intent)
+                        }
+                    )
+                },
+                bottomBar = {
+                    CompositionLocalProvider(
+                        LocalContentColor provides Color.White
+                    ) {
+                        Surface(color = appBarColor) {
+                            BottomAppBar {
+                                // Contenido del BottomAppBar
+                            }
+                        }
+                    }
+                }
+            ) { innerPadding ->
 
-            Image(
-                painter = painterResource(id = imageResId),
-                contentDescription = null,
-                modifier = Modifier
-                    .size(200.dp)
-                    .padding(8.dp)
 
-            )
+                Column(
+                    modifier = Modifier
+                        .padding(innerPadding) // Aquí aplicamos el innerPadding
+                        .padding(16.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
 
-            Spacer(modifier = Modifier.height(16.dp))
 
-            val imcData = listOf(
-                Triple("Menos de 18.5", "Bajo peso", "Sería recomendable acudir a un nutricionista."),
-                Triple("18.5 - 24.9", "Normal", "Mantén una dieta equilibrada."),
-                Triple("25 - 29.9", "Sobrepeso", "Considera hacer más ejercicio."),
-                Triple("30 o más", "Obesidad", "Consulta a un especialista.")
-            )
+                    Image(
+                        painter = painterResource(id = imageResId),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(200.dp)
+                            .padding(8.dp)
 
-            imcData.forEach { data ->
-                Text(text = data.first, color = secundaryColor) // Set the color of the text
-                Spacer(modifier = Modifier.height(8.dp))
-                Text(text = "${data.second}, ${data.third}", color = principalColor) // Set the color of the text
-                Spacer(modifier = Modifier.height(16.dp))
+                    )
+
+                    Spacer(modifier = Modifier.height(16.dp))
+
+                    val imcData = listOf(
+                        Triple(
+                            "Menos de 18.5",
+                            "Bajo peso",
+                            "Sería recomendable acudir a un nutricionista."
+                        ),
+                        Triple("18.5 - 24.9", "Normal", "Mantén una dieta equilibrada."),
+                        Triple("25 - 29.9", "Sobrepeso", "Considera hacer más ejercicio."),
+                        Triple("30 o más", "Obesidad", "Consulta a un especialista.")
+                    )
+
+                    imcData.forEach { data ->
+                        Text(text = data.first, color = secundaryColor) // Set the color of the text
+                        Spacer(modifier = Modifier.height(8.dp))
+                        Text(
+                            text = "${data.second}, ${data.third}",
+                            color = principalColor
+                        ) // Set the color of the text
+                        Spacer(modifier = Modifier.height(16.dp))
+                    }
+                }
             }
         }
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun CustomTopAppBar(title: String, backgroundColor: Color, onBack: () -> Unit) {
+        SmallTopAppBar(
+            title = {
+                Box(
+                    modifier = Modifier.fillMaxWidth(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(text = title, color = Color.White) // Establece el color del texto directamente
+                }
+            },
+            navigationIcon = {
+                IconButton(onClick = onBack) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = "Back",
+                        tint = Color.White // Establece el color del ícono directamente
+                    )
+                }
+            },
+            colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = backgroundColor)
+        )
     }
 
     @Preview
